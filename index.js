76a492f05c6175d25a2cfd063184f1513f222b4a
Jeff Walton <jwalton89@gmail.com>
2017-11-25 02:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+
+
+function sayHiToGrandma(string) {
+  return (if (lowercase === 'hello'
+}
