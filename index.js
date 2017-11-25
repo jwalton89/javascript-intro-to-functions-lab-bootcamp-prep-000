a6f37f76fc08bcd10770117ec1fafde97f24cf23
Jeff Walton <jwalton89@gmail.com>
2017-11-25 02:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+function sayHiToGradma(string) {
+  console.log(if (lowercase.ToLowerCase()===lowercase) {console.log("I can't hear you!")}
+}
