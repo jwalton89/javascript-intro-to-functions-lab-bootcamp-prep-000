b19842e68ad10e87a816d05c5be4a4e27c58a8aa
Jeff Walton <jwalton89@gmail.com>
2017-11-25 03:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {return "I can't hear you!"}
-  else if (string === string.toUpperCase()) {console.log("YES INDEED!")}
-  else {console.log("I love you, too.")}
+  else if (string === string.toUpperCase()) {return "YES INDEED!"}
+  else {return "I love you, too."}
 }
